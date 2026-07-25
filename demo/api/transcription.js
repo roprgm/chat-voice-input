2127d5e9a57c8e4b1db358ece4ede27d72eca873
Rodrigo <roprgm@gmail.com>
2026-07-25 23:19:35 +0300

    Use compatible Vercel handler

--- demo/api/transcription.js
+++ demo/api/transcription.js
@@ -1,6 +1,6 @@
 import { createTranscriptionTokenResponse } from "chat-voice-input/server";
 
-export function POST(): Promise<Response> {
+export function POST() {
   return createTranscriptionTokenResponse({
     apiKey: process.env.AI_GATEWAY_API_KEY,
   });
